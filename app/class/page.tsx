2f59f84db2e2d1ecdd883c1ffbf94be828a4e392
oartuu <artu.donascimento@gmail.com>
2026-05-21 16:46:54 -0300

    feat(class):update class page

--- app/class/page.tsx
+++ app/class/page.tsx
@@ -8,11 +8,12 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { CirclePlus, School } from "lucide-react";
+import { ChevronRight, CirclePlus, School } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { getToken,logout } from "@/lib/auth";
+import { Separator } from "@/components/ui/separator";
 
 
 type Class = {
@@ -110,38 +111,49 @@ export default function Home() {
 
   return (
     <div className="h-dvh flex flex-col bg-zinc-100 dark:bg-zinc-900">
-      <header className="bg-zinc-800 w-full h-18 flex justify-center items-center shadow-md">
+      <header className="bg-zinc-800 w-full h-18 px-2 flex justify-between items-center shadow-md">
+        <div className="">
+          <img src="beacon.svg" alt="aaaaaaaaa" className="h-12" />
+        </div>
         <div>
           <input
-            className="bg-zinc-50 text-zinc-950 w-xl p-2 rounded-sm shadow-"
+            className="bg-zinc-50 text-zinc-950 w-xl p-2 rounded-sm shadow-md "
             type="text"
             placeholder="pesquisar turma"
           />
         </div>
+        <Button
+          size={"lg"}
+          onClick={() => setIsDialogOpen(true)}
+          className=" flex justify-between hover:cursor-pointer hover:text-zinc-100 bg-zinc-100 text-zinc-900 pr-4"
+        >
+          Nova Turma <CirclePlus />
+        </Button>
       </header>
 
-      <main className="flex-1 flex flex-wrap items-start content-start  gap-2  p-4 overflow-auto">
-        {classes.map((c) => (
-          <Card
-            onClick={() => navigateToClass(c.id)}
-            key={c.id}
-            className="w-1/6  hover:cursor-pointer"
-          >
-            <CardContent className="flex flex-col gap-4 items-center justify-center">
-              <p>{c.name}</p>
-              <School />
-            </CardContent>
-          </Card>
-        ))}
-        <Card
-          onClick={() => setIsDialogOpen(true)}
-          className="w-1/6 bg-transparent border-2 border-zinc-950 border-dashed hover:cursor-pointer"
-        >
-          <CardContent className="flex flex-col gap-4 items-center justify-center">
-            <p>ADD TURMA</p>
-            <CirclePlus />
-          </CardContent>
-        </Card>
+      <main className="flex-1 flex flex-col flex-wrap items-start content-start  gap-2  p-4 overflow-auto">
+        <h1 className="px-4 mt-2">Turmas cadastradas: </h1>
+        <Separator className="dark:bg-zinc-500" />
+        <div className=" w-full flex flex-wrap items-start content-start  gap-2  overflow-auto">
+          {classes.map((c) => (
+            <Card
+              onClick={() => navigateToClass(c.id)}
+              key={c.id}
+              className="w-3/12 border dark:bg-zinc-900 border-zinc-200 dark:border-zinc-500 hover:cursor-pointer"
+            >
+              <CardContent className="flex gap-4 items-center justify-between ">
+                <div className="flex gap-2">
+                  <p className="text-base">{c.name}</p>
+                  <School />
+                </div>
+                <div className="flex gap-2">
+                  <Separator orientation="vertical" />
+                  <ChevronRight />
+                </div>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
 
         <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
           <DialogContent>
